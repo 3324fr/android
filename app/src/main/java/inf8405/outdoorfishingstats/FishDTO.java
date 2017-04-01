f457be06c5c0ab4407b0e3ec3f3dc43099670a78
Sylvester <sylvester.vuong@gmail.com>
2017-04-01 16:57:35 -0400

    fishDTO

---
+++ app/src/main/java/inf8405/outdoorfishingstats/FishDTO.java
@@ -0,0 +1,18 @@
+package inf8405.outdoorfishingstats;
+
+import android.hardware.Sensor;
+
+/**
+ * Created by Sly on 4/1/2017.
+ */
+
+public class FishDTO {
+
+    public String name;
+    public String contact;
+    public String time;
+    public double longitude;
+    public double latitude;
+
+
+}
