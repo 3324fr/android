43076b5208f9207e04883d249c7fe2ac109b19d1
lam <ba.samson.lam@gmail.com>
2017-04-15 22:59:09 -0400

    nfc marche

--- app/src/main/java/inf8405/outdoorfishingstats/BeamActivity.java
+++ app/src/main/java/inf8405/outdoorfishingstats/BeamActivity.java
@@ -1,13 +1,7 @@
 package inf8405.outdoorfishingstats;
 
-import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,55 +16,43 @@ import android.os.Parcelable;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
-import java.util.Arrays;
 
 public class BeamActivity  extends AppCompatActivity  implements CreateNdefMessageCallback{
 
     NfcAdapter mNfcAdapter;
     TextView textView;
     public static final String PREFS_KEY = "displayName";
-    public static final String MIME_TEXT_PLAIN = "text/plain";
-    public static final String TAG = "NfcDemo";
-    private TextView mTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beam);
-
-        mTextView = (TextView) findViewById(R.id.textnfc);
-
+        TextView textView = (TextView) findViewById(R.id.textnfc);
+        // Check for available NFC Adapter
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
         if (mNfcAdapter == null) {
-            // Stop here, we definitely need NFC
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
             finish();
             return;
-
         }
-
-        if (!mNfcAdapter.isEnabled()) {
-            mTextView.setText("NFC is disabled.");
-        } else {
-            mTextView.setText("GOOD THING");
-        }
-
-        handleIntent(getIntent());
+        // Register callback
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
+        textView = (TextView) findViewById(R.id.textnfc);
+        textView.setText(" TV before");
     }
 
     @Override
     public NdefMessage createNdefMessage(NfcEvent event) {
-        String text = ("Beam me up, Android!\n\n" +
-                "Beam Time: " + System.currentTimeMillis());
+        //String text = ("Beam me up, Android!\n\n" + "Beam Time: " + System.currentTimeMillis());
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String text = (pref.getString(FishActivity.PREFS_KEY, getResources().getString(R.string.pref_default_display_name)));
         //Toast.makeText(this,"Username " + getDisplayNamePreference(getApplicationContext()) , Toast.LENGTH_SHORT).show();
         String userName = FishActivity.m_displayName == null ? getDisplayNamePreference(getApplicationContext()) : FishActivity.m_displayName;
         NdefMessage msg = new NdefMessage(
                 new NdefRecord[] { NdefRecord.createMime(
-                        "" + userName, text.getBytes())
+                        userName, text.getBytes())
                         /**
                          * The Android Application Record (AAR) is commented out. When a device
                          * receives a push with an AAR in it, the application specified in the AAR
@@ -89,58 +71,18 @@ public class BeamActivity  extends AppCompatActivity  implements CreateNdefMessa
         super.onResume();
         // Check to see that the Activity started due to an Android Beam
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            Log.d(TAG, "OnResume ==== ");
             processIntent(getIntent());
-            handleIntent(getIntent());
         } else {
-            Log.d(TAG, "intent");
+            processIntent(getIntent());
         }
-        setupForegroundDispatch(this, mNfcAdapter);
     }
-
-    @Override
-    protected void onPause() {
-        /**
-         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
-         */
-        stopForegroundDispatch(this, mNfcAdapter);
-
-        super.onPause();
-    }
-
-    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        adapter.disableForegroundDispatch(activity);
-    }
-
 
     @Override
     public void onNewIntent(Intent intent) {
         // onResume gets called after this to handle the intent
         setIntent(intent);
-        handleIntent(intent);
     }
 
-    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-
-        IntentFilter[] filters = new IntentFilter[1];
-        String[][] techList = new String[][]{};
-
-        // Notice that this is the same filter as in our manifest.
-        filters[0] = new IntentFilter();
-        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-        try {
-            filters[0].addDataType(MIME_TEXT_PLAIN);
-        } catch (IntentFilter.MalformedMimeTypeException e) {
-            throw new RuntimeException("Check your mime type.");
-        }
-
-        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-    }
     /**
      * Parses the NDEF Message from the intent and prints to the TextView
      */
@@ -157,113 +99,18 @@ public class BeamActivity  extends AppCompatActivity  implements CreateNdefMessa
             for (int i = 0; i < rawMsgs.length; i++) {
                 messages[i] = (NdefMessage) rawMsgs[i];
                 msgBuilder+= new String(messages[i].getRecords()[0].getPayload());
+
             }
         }
         else {
             msgBuilder = "erreur";
         }
         // record 0 contains the MIME type, record 1 is the AAR, if present
-        Log.d("InProcess","weird stuff happening");
+        MainActivity.listeAmis.add(msgBuilder);
 
         textView.setText(msgBuilder);
     }
 
-    private void handleIntent(Intent intent) {
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)) {
-
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                new NdefReaderTask().execute(tag);
-
-            } else {
-                Log.d(TAG, "Wrong mime type: " + type);
-            }
-        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-
-            // In case we would still use the Tech Discovered Intent
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String[] techList = tag.getTechList();
-            String searchedTech = Ndef.class.getName();
-
-            for (String tech : techList) {
-                if (searchedTech.equals(tech)) {
-                    new NdefReaderTask().execute(tag);
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * Background task for reading the data. Do not block the UI thread while reading.
-     *
-     * @author Ralf Wondratschek
-     *
-     */
-    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-
-        @Override
-        protected String doInBackground(Tag... params) {
-            Tag tag = params[0];
-
-            Ndef ndef = Ndef.get(tag);
-            if (ndef == null) {
-                // NDEF is not supported by this Tag.
-                return null;
-            }
-
-            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
-            NdefRecord[] records = ndefMessage.getRecords();
-            for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                    try {
-                        return readText(ndefRecord);
-                    } catch (UnsupportedEncodingException e) {
-                        Log.e(TAG, "Unsupported Encoding", e);
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        /*
-         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-         *
-         * http://www.nfc-forum.org/specs/
-         *
-         * bit_7 defines encoding
-         * bit_6 reserved for future use, must be 0
-         * bit_5..0 length of IANA language code
-         */
-
-            byte[] payload = record.getPayload();
-
-            // Get the Text Encoding
-            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-            // Get the Language Code
-            int languageCodeLength = payload[0] & 0063;
-
-            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-            // e.g. "en"
-
-            // Get the Text
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                textView.setText("Read content: " + result);
-            }
-        }
-    }
 
     private String getDisplayNamePreference(Context context){
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
